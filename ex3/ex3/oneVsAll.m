8cd354ba8912945cf056022a3fd7b8c7ce977010
Ishan Manchanda <ishanmanchanda70@gmail.com>
2021-07-17 21:38:46 +0530

    Minor refactors for ex3

--- ex3/ex3/oneVsAll.m
+++ ex3/ex3/oneVsAll.m
@@ -49,7 +49,6 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
-
 % Set Initial theta
 initial_theta = zeros(n + 1, 1);
 
@@ -58,17 +57,10 @@ options = optimset('GradObj', 'on', 'MaxIter', 50);
  
 % Run fmincg to obtain the optimal theta
 % This function will return theta and the cost
-
-
 for i = 1:num_labels
     [theta, ~] = fmincg (@(t)(lrCostFunction(t, X, (y == i), lambda)), initial_theta, options);
     all_theta(i, 1:end) = theta;
 end
-
-
-
-
-
 
 % =========================================================================
 
