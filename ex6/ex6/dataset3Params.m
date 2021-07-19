f5335148138e260aa09980ed129b053501cef6a2
Ishan Manchanda <ishanmanchanda70@gmail.com>
2021-07-19 19:04:52 +0530

    Added solutions for assignment 6

--- ex6/ex6/dataset3Params.m
+++ ex6/ex6/dataset3Params.m
@@ -23,11 +23,26 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_vals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigma_vals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+C_count = size(C_vals, 2);
+sigma_count = size(sigma_vals, 2);
 
+errors = zeros(C_count * sigma_count, 3);
 
+for i = 1:C_count
+   for j = 1:sigma_count
+       model = svmTrain(X, y, C_vals(i), @(x1, x2) gaussianKernel(x1, x2, sigma_vals(j)));
+       predictions = svmPredict(model, Xval);
+       error = mean(double(predictions ~= yval));
+       errors(8 * i + j - 8, :) = [error, C_vals(i), sigma_vals(j)];
+   end
+end
 
-
+sorted = sortrows(errors);
+C = sorted(1, 2);
+sigma = sorted(1, 3);
 
 % =========================================================================
 
